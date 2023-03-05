903d721c119e57dfefb224e58d7ce7bca8c3c759
Jose Sosa <jose@centauri>
2023-03-05 00:53:29 -0600

    ETHDenver Hackaton

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# staking-pool-aurora
-Meta Staking pool in Aurora
+# Staking Pool Aurora
+
+Meta Staking pool in Aurora.
+
+# Aurora Bounty ETHDenver2023
+
+We have been working on the stAUR, which is the liquid staking token that represent a proportion of the total Auroras staked into Aurora.plus by the N number of depositors. A good number of depositors to start with is with 3.
+
+The **Depositors** are smart contracts that deposit Aurora into the staking service. The objective of spliting the deposits into multiple depositors is to allow deposits from one depositor, keeping the other two of them without the redeem penalization.
+
+Three different contracts are needed to be deployed.
+
+- The stAUR fungible token: ERC20, ERC4626.
+- The staking manager is the contract than contains all the logic to stake, unstake and the stAUR-AURORA liquidity pool.
+- The depositors are separated smart contracts that have the logic to deposit and withdraw from the Aurora plus staking service.
+
+Using the Aurora SDK, after the liquidity pool is developed, the stAUR token could be used directy in Meta Yield.
+
+![Architecture](media/stakingAurora.png)
+
+First, the contracts will be deployed in ETH Goerli, using the two `testing/` contracts for the Aurora Staking service and the Aurora token.
+
+
+
+Addresses of the deployed contracts:
+ - Token:             0xB0Ac0da82FF21027D1d1aB13AE67e9C7953AA66e
+ - AuroraStaking:     0xba63a349Fc594B2255e9541C49DA58fAf7a2A53C
+ - StakingManager:    0x2C5981D82Ca960602B2e829a0DAa1bDA56CA2c44
+ - Depositor 00:      0xBFfcc6517e372bF28909940A5Ee49b5AE4C8d466
+ - Depositor 01:      0x244AfCd5a0bc8A4400c6702C6a2A7717945c5e70
+ - StakedAuroraVault: 0x0f4967Ff6387798958fF143DAA00E0D98Fd26b46
--- scripts/test_deploy.js
+++ scripts/test_deploy.js
@@ -2,10 +2,6 @@
 
 async function main() {
 
-  const MAINNET_AURORA_PLUS = "0xccc2b1aD21666A5847A804a73a41F904C4a4A0Ec";
-  const MAINNET_AURORA_TOKEN = "0x8BEc47865aDe3B172A928df8f990Bc7f2A3b9f79";
-  const decimals = ethers.BigNumber.from(10).pow(18);
-
   const AuroraToken = await ethers.getContractFactory("Token");
   const AuroraStaking = await ethers.getContractFactory("AuroraStaking");
   const StakingManager = await ethers.getContractFactory("StakingManager");
@@ -16,37 +12,63 @@ async function main() {
     bob
   ] = await ethers.getSigners();
 
-  // const Token = await ethers.getContractFactory('Token');
-  // const token = await Token.attach('0x8BEc47865aDe3B172A928df8f990Bc7f2A3b9f79')
-  // console.log("jose aurora tokens: %s", await token.balanceOf("0x0B438De1DCa9FBa6D14F17c1F0969ECc73C8186F"));
+  // Step 1. Deploying a dummy token for testing Aurora.
+  console.log("Step 1. Deploying AuroraToken...")
+  const decimals = ethers.BigNumber.from(10).pow(18);
+  const initialSupply = ethers.BigNumber.from(10_000_000).mul(decimals);
+  const auroraTokenContract = await AuroraToken.connect(alice).deploy(
+    initialSupply,
+    "Aurora Token",
+    "AURORA",
+    alice.address
+  );
+  await auroraTokenContract.deployed();
+  console.log("       ...done!");
 
-  // Step 1. Deploying the IMMUTABLE Staked Aurora Vault contract.
-  console.log("Step 1. Deploying StakedAuroraVault...")
+  // Sharing total suply with Bob.
+  const splitSupply = ethers.BigNumber.from(3_000_000).mul(decimals);
+  await auroraTokenContract.connect(alice).transfer(bob.address, splitSupply);
+  // await auroraTokenContract.connect(alice).transfer(carl.address, splitSupply);
+
+  // Step 2. Deploying a dummy staking service.
+  console.log("Step 2. Deploying AuroraStaking...")
+  const auroraStakingContract = await AuroraStaking.connect(alice).deploy(
+    auroraTokenContract.address
+  );
+  await auroraStakingContract.deployed();
+  console.log("       ...done!");
+
+  // Send Tokens to the Aurora Staking contract to pay for rewards.
+  const forRewards = ethers.BigNumber.from(1_000_000).mul(decimals);
+  await auroraTokenContract.connect(alice).transfer(auroraStakingContract.address, forRewards);
+
+  // Step 3. Deploying the IMMUTABLE Staked Aurora Vault contract.
+  console.log("Step 3. Deploying StakedAuroraVault...")
   const minDepositAmount = ethers.BigNumber.from(1).mul(decimals);
   const stakedAuroraVaultContract = await StakedAuroraVault.connect(alice).deploy(
-    MAINNET_AURORA_TOKEN,
+    auroraTokenContract.address,
     "Staked Aurora Token",
     "stAUR",
     minDepositAmount
   );
   await stakedAuroraVaultContract.deployed();
-  console.log(" ...done in %s!", stakedAuroraVaultContract.address);
+  console.log("       ...done!");
 
-  // Step 2. Deploying the MUTABLE Staking Manager contract.
-  console.log("Step 2. Deploying StakingManager...")
+  // Step 4. Deploying the MUTABLE Staking Manager contract.
+  console.log("Step 4. Deploying StakingManager...")
   const stakingManagerContract = await StakingManager.connect(alice).deploy(
     stakedAuroraVaultContract.address,
-    MAINNET_AURORA_PLUS,
+    auroraStakingContract.address,
     bob.address,
     10
   );
   await stakingManagerContract.deployed();
-  console.log(" ...done in %s!", stakingManagerContract.address);
+  console.log("       ...done!");
 
   // Insert/update the staking manager in the ERC-4626
   await stakedAuroraVaultContract.updateStakingManager(stakingManagerContract.address);
 
-  // Step 3. Deploying the multiple Depositor contracts.
+  // Step 5. Deploying the multiple Depositor contracts.
   console.log("Step 5. Deploying 2 Depositor contracts...")
   const depositor00Contract = await Depositor.connect(bob).deploy(
     stakingManagerContract.address
@@ -63,8 +85,8 @@ async function main() {
   await stakingManagerContract.connect(bob).insertDepositor(depositor01Contract.address);
 
   console.log("Addresses of the deployed contracts:")
-  console.log(" - Token:             %s", MAINNET_AURORA_TOKEN);
-  console.log(" - AuroraStaking:     %s", MAINNET_AURORA_PLUS);
+  console.log(" - Token:             %s", auroraTokenContract.address);
+  console.log(" - AuroraStaking:     %s", auroraStakingContract.address);
   console.log(" - StakingManager:    %s", stakingManagerContract.address);
   console.log(" - Depositor 00:      %s", depositor00Contract.address);
   console.log(" - Depositor 01:      %s", depositor01Contract.address);
